f8c16a4b5a5d3aeddced02e7f2a54874be924f3e
Emmanuel-Ejeagha <nuel6700@gmail.com>
2025-01-11 22:48:45 +0100

    Modified database

--- obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eeabd5ab44221250d172364637494fd39b0b6a0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32335dc097933f451a167a623dc3041a19010bb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
