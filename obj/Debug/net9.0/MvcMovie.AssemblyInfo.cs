eeabd5ab44221250d172364637494fd39b0b6a0b
Emmanuel-Ejeagha <nuel6700@gmail.com>
2025-01-11 21:00:03 +0100

    ASP.NET Core MVC

--- obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c5790d652158ad093fba1f156359b41ebac7f1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d828d8b1e2489c16c4a210306dd7ffe76d6f906")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
