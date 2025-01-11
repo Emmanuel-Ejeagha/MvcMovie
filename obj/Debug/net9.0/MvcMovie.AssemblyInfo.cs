def88914b0fd1ebc571295e5e00f1f3d03f93338
Emmanuel-Ejeagha <nuel6700@gmail.com>
2025-01-12 00:21:59 +0100

    Add: HttpPost Index

--- obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97d923c73df1480eb5ee5c6825719f47c283e3cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8efa715fd79a0331b095ece1fa4e0c53e19e7f1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
